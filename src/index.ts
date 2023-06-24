3b2bf35a6c60bd984e149a6c190f39f3e4b9dbee
Joao-Ortega <joao_ortega17@hotmail.com>
2023-06-24 13:32:33 -0300

    configurating cors

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 import express from "express";
+import cors from 'cors';
 import router from "./routes/index.routes";
 import './database/mongoConfig';
 
+const corsOptions = {
+  origin: 'http://localhost:3000'
+};
+
 const app = express()
 app.use(express.json())
+app.use(cors(corsOptions))
 app.use(router)
 
 app.listen(process.env.PORT, () => {
